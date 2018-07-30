04b65d65763ceaa6e9fbeff22ae0cd5272857dfe
Wang <xiaoWangHi@outlook.com>
2018-07-30 19:20:49 +0800

    .

---
+++ src/assets/js/config.js
@@ -0,0 +1,20 @@
+/**
+ * 请求地址全部写在这里，定成常量，在各页面获取
+ **/
+
+/**
+ * 请求地址
+ **/ 
+const BASE_URL = "https://api.logoliqp.com";//测试本地服
+// var BASE_URL = "https://api.xiudian.com";//正式服
+
+
+/**
+ * 请求路径
+ **/
+const api = {
+
+    // 用户登录
+    API_LOGIN : BASE_URL + '/login',
+
+}
